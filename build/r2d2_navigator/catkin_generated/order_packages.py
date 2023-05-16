13347e33f4163b37b47fefb427db3da9eac59686
brvane <brvane@taltech.ee>
2023-05-16 14:14:43 +0300

    changes in urdf file and launch file

---
+++ build/r2d2_navigator/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/vboxuser/brita_met0310_av/src/r2d2_navigator'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/vboxuser/brita_met0310_av/devel;/opt/ros/noetic'.split(';') if '/home/vboxuser/brita_met0310_av/devel;/opt/ros/noetic' != '' else []
